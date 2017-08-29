248765630d9f26c723bdab212af9fa494413a358
Informática <ramon@comperve.ufrn.br>
2017-08-29 18:23:52 -0300

    Primeiro commit no repositório

---
+++ src/br/com/imd/protocolos/udp/echo/main/ClienteUDP.java
@@ -0,0 +1,36 @@
+package br.com.imd.protocolos.udp.echo.main;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class ClienteUDP {
+
+	static Scanner leitorTeclado = new Scanner(System.in);
+	
+	public static void main(String[] args) throws Exception {
+		int porta = 7777;
+		String mensagem = "";
+		
+		System.out.print("Digite a mensagem para enviar ao servidor: ");
+		mensagem = leitorTeclado.nextLine();
+
+		byte[] dadosEnviados = mensagem.getBytes();
+		byte[] dadosRecebidos = new byte[512];
+
+		InetAddress hostDestino = InetAddress.getByName("localhost");
+
+		DatagramSocket socketCliente = new DatagramSocket();
+		DatagramPacket pacoteEnviado = new DatagramPacket(dadosEnviados, dadosEnviados.length, hostDestino, porta);
+
+		socketCliente.send(pacoteEnviado);
+
+		DatagramPacket pacoteRespostaRecebida = new DatagramPacket(dadosRecebidos, dadosRecebidos.length);
+		socketCliente.receive(pacoteRespostaRecebida); // método bloqueante
+		String mensagemResposta = new String(dadosRecebidos);
+		System.out.println("Resposta do servidor: " + mensagemResposta);
+
+		socketCliente.close();
+	}
+}
---
+++ src/br/com/imd/protocolos/udp/echo/main/ServidorUDP.java
@@ -0,0 +1,31 @@
+package br.com.imd.protocolos.udp.echo.main;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class ServidorUDP {
+	public static void main(String[] args) throws Exception {
+		int porta = 7777;
+		byte[] dadosRecebidos = new byte[512];
+		byte[] dadosResposta;
+		
+		DatagramSocket socketServidor = new DatagramSocket(porta);
+		System.out.println("Servidor UDP em execução na porta " + porta + "...");
+
+		DatagramPacket pacoteRecebido = new DatagramPacket(dadosRecebidos, dadosRecebidos.length);
+		socketServidor.receive(pacoteRecebido); // método bloqueante
+
+		System.out.println(new String(dadosRecebidos));
+
+		dadosResposta = new String(dadosRecebidos).getBytes();
+		
+		InetAddress hostDestinoResposta = pacoteRecebido.getAddress();
+		int portaDestinoResposta = pacoteRecebido.getPort();
+		
+		DatagramPacket pacoteResposta = new DatagramPacket(dadosResposta, dadosResposta.length, hostDestinoResposta, portaDestinoResposta);
+		socketServidor.send(pacoteResposta);
+
+		socketServidor.close();
+	}
+}
