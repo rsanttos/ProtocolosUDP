da6c93716285c532a02e0cc59830d94c3022430a
Ramon Malaquias <rsanttos22@gmail.com>
2017-09-12 21:55:11 -0300

    Início da implementação do protocolo para envio de imagens segmentadas

--- src/br/com/imd/protocolos/udp/multicast/camera/ClienteUDP.java
+++ src/br/com/imd/protocolos/udp/multicast/camera/ClienteUDP.java
@@ -1,5 +1,6 @@
 package br.com.imd.protocolos.udp.multicast.camera;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,6 +13,8 @@ import java.util.Scanner;
 public class ClienteUDP {
 
 	static Scanner leitorTeclado = new Scanner(System.in);
+	
+	public static int LIMITE_TEORICO = 64000;
 
 	public static void main(String[] args) throws Exception {
 		int portaServidor = 4443;
@@ -21,19 +24,66 @@ public class ClienteUDP {
 
 		socketGrupo.joinGroup(enderecoGrupo);
 
-		byte[] dadosRecebidos = new byte[512];
+		byte[] dadosRecebidos = new byte[LIMITE_TEORICO];
+		byte[] dadosTotaisImagem;
+		ByteArrayOutputStream arrayAux = new ByteArrayOutputStream();
 
 		DatagramPacket pacoteRecebido = new DatagramPacket(dadosRecebidos, dadosRecebidos.length);
+
+		int tamanhoTotalPacote = 0;
+		int qtdPedacos = 0;
+		String milissegundos = "";
+		
 		while (true) {
 			socketGrupo.receive(pacoteRecebido);
-
-			lerArquivoRecebido(dadosRecebidos);
+			String mensagem = new String(dadosRecebidos);
+			
+			// Lendo mensagem inicial no formato TAMANHO_#_MILISSEGUNDOS
+			if(isMensagemInicial(mensagem)){
+				tamanhoTotalPacote = retornaTamanhoMensagemInicial(mensagem);
+				qtdPedacos = tamanhoTotalPacote / LIMITE_TEORICO;
+				milissegundos = retornaMilissegundosMensagemInicial(mensagem);
+			} else {
+				arrayAux.write(dadosRecebidos);
+				dadosTotaisImagem = arrayAux.toByteArray();
+				
+				if(dadosTotaisImagem.length >= tamanhoTotalPacote){
+					lerArquivoRecebido(dadosTotaisImagem);
+					arrayAux = new ByteArrayOutputStream();
+					dadosTotaisImagem = null;
+				}
+			}
 		}
+	}
+	
+	public static int retornaTamanhoMensagemInicial(String mensagemInicial){
+		String tamanho = "";
+		int indice = mensagemInicial.indexOf("_#_");
+		
+		for(int i = 0 ; i < indice ; i++){
+			tamanho += mensagemInicial.charAt(i);
+		}
+		System.out.println(tamanho);
+		return Integer.valueOf(tamanho);
+	}
 
+	public static String retornaMilissegundosMensagemInicial(String mensagemInicial){
+		String milissegundos = "";
+		int indice = mensagemInicial.indexOf("_#_");
+		
+		for(int i = indice + 3 ; i < mensagemInicial.length() ; i++){
+			milissegundos += mensagemInicial.charAt(i);
+		}
+		System.out.println(milissegundos);
+		return milissegundos;
+	}
+	
+	public static boolean isMensagemInicial(String mensagem){		
+		return mensagem.contains("_#_");
 	}
 
 	public static void lerArquivoRecebido(byte[] dadosRecebidos) throws IOException {
-		File arquivoRecebido = new File("arquivorecebido.txt");
+		File arquivoRecebido = new File("arquivorecebidodenovo.png");
 		OutputStream os = new FileOutputStream(arquivoRecebido);
 		os.write(dadosRecebidos);
 		os.flush();
--- src/br/com/imd/protocolos/udp/multicast/camera/ServidorUDP.java
+++ src/br/com/imd/protocolos/udp/multicast/camera/ServidorUDP.java
@@ -1,5 +1,6 @@
 package br.com.imd.protocolos.udp.multicast.camera;
 
+import java.awt.Dimension;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,12 +13,22 @@ import java.util.Date;
 import javax.imageio.ImageIO;
 
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
 
 public class ServidorUDP {
 	@SuppressWarnings("deprecation")
+	
+	public static int LIMITE_TEORICO = 64000;
+	
 	public static void main(String[] args) throws Exception {
 		int porta = 3737;
-		byte[] dadosEnvio;
+		
+		// Primeiros dados a serem enviados 
+		// Formato: TAMANHO_#_MILISSEGUNDOS
+		byte[] dadosIniciaisMensagem;
+		
+		byte[] dadosTotaisImagem;
+		byte[] dadosParciais;
 
 		DatagramSocket socketServidor = new DatagramSocket(porta);
 		System.out.println("Servidor UDP em execução na porta " + porta + "...");
@@ -31,25 +42,39 @@ public class ServidorUDP {
 					data.getMinutes(), data.getSeconds());
 			milissegundos = milissegundos / 1000;
 
-			// dadosEnvio = String.valueOf(milissegundos).getBytes();
+			File imagemGerada = geraImagem();
 
-			File imagemGerada = new File("teste.txt");
-
-			dadosEnvio = transformaArquivoEmByte(imagemGerada);
-
-			System.out.println(dadosEnvio.length);
+			dadosTotaisImagem = transformaArquivoEmByte(imagemGerada);
 			
-			DatagramPacket pacoteEnviado = new DatagramPacket(dadosEnvio, dadosEnvio.length, enderecoGrupoClientes,
-					portaClientes);
+			int tamanhoTotalPacote = dadosTotaisImagem.length;
+			int qtdPedacos = tamanhoTotalPacote/LIMITE_TEORICO;
 			
-			System.out.println(pacoteEnviado.getLength());
-
+			String mensagemInicial = String.valueOf(tamanhoTotalPacote) + "_#_" + milissegundos;
+			
+			dadosIniciaisMensagem = mensagemInicial.getBytes();
+			
+			// Enviando mensagem inicial
+			DatagramPacket pacoteEnviado = new DatagramPacket(dadosIniciaisMensagem, dadosIniciaisMensagem.length, enderecoGrupoClientes,
+					portaClientes);	
 			socketServidor.send(pacoteEnviado);
+			System.out.println("Mensagem inicial: " + mensagemInicial);
 			
-			System.out.println(socketServidor.getSendBufferSize());
-			System.out.println("Data: " + data + " enviada no formato: " + milissegundos);
+			// Preparando mensagem com a imagem
+			int indiceDadosTotais = 0;			
+			for(int i = 0 ; i < qtdPedacos ; i++){
+				int j = 0;
+				dadosParciais = new byte[LIMITE_TEORICO];
+				while(j < LIMITE_TEORICO && indiceDadosTotais < tamanhoTotalPacote){
+					dadosParciais[j] = dadosTotaisImagem[indiceDadosTotais];
+					indiceDadosTotais++;
+					j++;
+				}							
+				pacoteEnviado = new DatagramPacket(dadosParciais, dadosParciais.length, enderecoGrupoClientes,
+						portaClientes);	
+				socketServidor.send(pacoteEnviado);
+			}
 
-			Thread.sleep(5000);
+			Thread.sleep(2000);
 		}
 	}
 
@@ -71,9 +96,12 @@ public class ServidorUDP {
 
 	public static File geraImagem() throws IOException {
 		Webcam webcam = Webcam.getDefault();
+		webcam.setCustomViewSizes(new Dimension[]{WebcamResolution.HD720.getSize()});
+		webcam.setViewSize(WebcamResolution.HD720.getSize());
 		webcam.open();
 		File imagemGerada = new File("imagem.png");
 		ImageIO.write(webcam.getImage(), "PNG", imagemGerada);
+		webcam.close();
 		return imagemGerada;
 	}
 }
